e16560b7b11576225331f9a6dd6a7e5656253bec
Raghav Mundhara <mundhararaghav16@gmail.com>
2026-06-16 16:58:39 +0530

    Added Product model And DB Connection

--- src/main/java/com/ecom/demo/DemoApplication.java
+++ src/main/java/com/ecom/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.ecom.demo;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
---
+++ src/main/java/com/ecom/demo/model/ProductModel.java
@@ -0,0 +1,31 @@
+package com.ecom.demo.model;
+
+import java.math.BigDecimal;
+import java.util.*;
+import org.springframework.stereotype.Component;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
+@Entity
+public class ProductModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private String category;
+    private Date releaseDate;
+    private boolean isAvailable;
+    private int stockQuantity;
+}
